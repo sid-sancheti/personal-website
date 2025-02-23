68a47a0744f93cc21d3f3c7ae6a985e4a60eff05
Sid Sancheti <sidsancheti94@gmail.com>
2025-02-22 20:53:33 -0500

    Header tags initialization

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,12 @@ export default function BaseLayout({
   }) {
     return (
       <html lang="en">
+        <head>
+        <meta charSet="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <title>Sid Sancheti's Website</title>
+            <link rel="icon" type="image/jpg" href="/SS.jpg" />
+        </head>
         <body>
           {/* Layout UI */}
           {/* Place children where you want to render a page or nested layout */}
