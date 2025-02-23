56c7cc7ee01d225ed59fd47ec8458518a6216895
Sid Sancheti <sidsancheti94@gmail.com>
2025-02-22 20:47:19 -0500

    Add blog post page and base layout; update .gitignore

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+// Base Layout with rectangle, header, footer, and sphere.
+export default function BaseLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <html lang="en">
+        <body>
+          {/* Layout UI */}
+          {/* Place children where you want to render a page or nested layout */}
+          <main>{children}</main>
+        </body>
+      </html>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+// About me page. Content that describes men and my interests.
+// Centered
+// Arrow points to the sphere with a special font (My first WebGL project!!!)
+
+export default function Page() {
+    return (
+        <p>
+            Greetings traveller 🚀. Its a pleasure to make your aquantaince in my little corner of the internet.
+            A bit about me... I am a student at Purdue University studying Computer Science with a focus in Security.
+            I am a lover of all things tech and am always looking for the next big thing. I dive into everything that
+            piques my interest in the moment, which is currently Graphics and Security.
+
+            Feel free to explore a little bit longer 😁...
+        </p>
+    );
+}
