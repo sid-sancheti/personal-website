f094b691d99adec7894ebef02e3e5ac7064a6a27
Sid Sancheti <sidsancheti94@gmail.com>
2025-03-17 00:11:01 -0400

    Updated the sphere; clipped points, added rotation, and made points circular

--- app/components/Sphere.tsx
+++ app/components/Sphere.tsx
@@ -2,7 +2,7 @@
 "use client"
 
 import * as THREE from "three";
-import React, { useMemo } from "react";
+import React, { useMemo, useRef, useFrame } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Points, PointMaterial } from "@react-three/drei";
 
@@ -33,21 +33,41 @@ function populateSpherePoints(): THREE.Vector3[] {
 
 const SpherePoints = () => {
   const points = useMemo(() => {
-    return new Float32Array(populateSpherePoints().flatMap(v => [v.x, v.y, v.z]));
+    return populateSpherePoints();
   }, []);
 
+  const clippedPoints = useMemo(() => {
+    const plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0.5); // Plane at z = 0.5
+    return points.filter((point) => plane.distanceToPoint(point) > 0);
+  }, [points]);
+
+  const float32Array = useMemo(() => {
+    return new Float32Array(clippedPoints.flatMap(v => [v.x, v.y, v.z]));
+  }, [clippedPoints]);
+
   return (
-    <Points positions={points} frustumCulled={false}>
-      <PointMaterial size={0.02} sizeAttenuation color="white" />
+    <Points positions={float32Array} frustumCulled={false}>
+      <PointMaterial size={0.02} sizeAttenuation color="white" transparent alphaTest={0.5} depthWrite={false}/>
     </Points>
   );
 };
 
 export default function Sphere() {
+  const sphereRef = useRef<THREE.Group>(null);
+  useFrame(() => {
+    if (sphereRef.current) {
+      sphereRef.current.rotation.x += 0.005;
+      sphereRef.current.rotation.y += 0.005;
+      sphereRef.current.rotation.z += 0.005;
+    }
+  });
+
   return (
     <Canvas camera={{ position: [0, 0, 2] }}>
       <ambientLight />
-      <SpherePoints />
+      <group ref={sphereRef}>
+        <SpherePoints />
+      </group>
     </Canvas>
   );
 }
